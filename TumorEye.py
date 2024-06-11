4fd515354bbba2d40e19e611635e5682e7453d78
nomadandy <ich.weiss.andreas@web.de>
2024-06-11 09:00:15 +0200

    Splitted training data and prediction data

--- TumorEye.py
+++ TumorEye.py
@@ -1,7 +1,7 @@
 #Import the dataset.
 #[Click here for the dataset](https://gist.github.com/KhanradCoder/35a6beea49e5b9ba62797e595a9626c0)
 import pandas as pd
-dataset = pd.read_csv('cancer.csv')
+dataset = pd.read_csv('cancer_cropped_dataset.csv')
 x = dataset.drop(columns=["diagnosis(1=m, 0=b)"])
 y = dataset["diagnosis(1=m, 0=b)"]
 
@@ -16,7 +16,7 @@ model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape[1:], activation='
 model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=1000)
+model.fit(x_train, y_train, epochs=100)
 
 #Evaluate the model.
 model.evaluate(x_test, y_test)
@@ -25,4 +25,9 @@ model.evaluate(x_test, y_test)
 candidates = pd.read_csv('TumorEye_Predict.csv')
 candidateList = candidates.drop(columns=["Candidate"])
 prediction = model.predict(candidateList)
+print("")
+print("Making Predictions")
+print("0.00 to 0.50: probably not malicious")
+print("0.51 to 1.00: probably malicious")
+print("One dataset per line:")
 print(prediction)
